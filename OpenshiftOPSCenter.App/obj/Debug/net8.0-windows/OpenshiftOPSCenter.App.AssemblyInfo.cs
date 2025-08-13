36b6d99d631821bfdc723ac5d122cb122a070018
Ixothron <>
2025-08-13 07:44:52 +0200

    Visual Studio Files

--- OpenshiftOPSCenter.App/obj/Debug/net8.0-windows/OpenshiftOPSCenter.App.AssemblyInfo.cs
+++ OpenshiftOPSCenter.App/obj/Debug/net8.0-windows/OpenshiftOPSCenter.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenshiftOPSCenter.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6df350d1d993ba175e38172a2b844b23a167b62b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenshiftOPSCenter.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenshiftOPSCenter.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
